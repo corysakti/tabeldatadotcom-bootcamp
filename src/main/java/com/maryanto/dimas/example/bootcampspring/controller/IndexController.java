6feb537d4a238fc5aec0cf7d539556674d069d75
dimasm93 <software.dimas_m@icloud.com>
2022-01-19 14:31:56 +0700

    membuat proses submit post

--- src/main/java/com/maryanto/dimas/example/bootcampspring/controller/IndexController.java
+++ src/main/java/com/maryanto/dimas/example/bootcampspring/controller/IndexController.java
@@ -3,8 +3,8 @@ package com.maryanto.dimas.example.bootcampspring.controller;
 import com.maryanto.dimas.example.bootcampspring.entity.Department;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
 
 @Controller
 @RequestMapping("/web")
@@ -14,5 +14,20 @@ public class IndexController {
     public String showIndex(Model model) {
         model.addAttribute("data", new Department());
         return "index";
+    }
+
+    @PostMapping("/test")
+    public String processRequest(
+            @RequestParam(name = "nama") String nama,
+            @RequestParam("description") String description) {
+        System.out.println("nama: " + nama + ", description: " + description);
+        return "redirect:/web/index";
+    }
+
+    @PostMapping("/test2")
+    public String processRequest2(
+            @ModelAttribute Department dept) {
+        System.out.println("nama: " + dept.getNama() + ", description: " + dept.getDescription());
+        return "redirect:/web/index";
     }
 }
