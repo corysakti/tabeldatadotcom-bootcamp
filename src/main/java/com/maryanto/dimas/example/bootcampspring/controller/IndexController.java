09bb1ae72d856b73ee54fe075f59a47a2cbb1678
dimasm93 <software.dimas_m@icloud.com>
2022-01-20 09:39:57 +0700

    update template'

--- src/main/java/com/maryanto/dimas/example/bootcampspring/controller/IndexController.java
+++ src/main/java/com/maryanto/dimas/example/bootcampspring/controller/IndexController.java
@@ -6,9 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
-
 
 @Controller
 @RequestMapping("/web")
@@ -35,8 +35,9 @@ public class IndexController {
 
     @PostMapping("/test2")
     public String processRequest2(
-            @ModelAttribute Department dept) {
+            @ModelAttribute Department dept, RedirectAttributes rattrs) {
         this.repo.insert(dept);
+        rattrs.addFlashAttribute("successInfo", true);
         return "redirect:/web/index";
     }
 }
